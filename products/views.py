05066ddb7080a059040522025c1dd8432769adf2
mariebernf <77eyo7ks@students.codeinstitute.net>
2026-01-08 17:34:51 +0100

    Refactor product views to remove duplicate logic

--- products/views.py
+++ products/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
-def all_products(request):
+def product_list(request):
     products = Product.objects.all()
-    context = {
-        'products': products,
-    }
-    return render(request, 'products/products.html', context)
+    return render(request, 'products/products.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'products/product_detail.html', {'product': product})
