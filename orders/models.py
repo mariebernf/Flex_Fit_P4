92ff9150bd0447fccdb14e62f52e74482c4c3741
mariebernf <77eyo7ks@students.codeinstitute.net>
2026-01-10 02:26:24 +0100

    Add order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    full_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    country = models.CharField(max_length=40)
+    postcode = models.CharField(max_length=20)
+    town_or_city = models.CharField(max_length=40)
+    street_address1 = models.CharField(max_length=80)
+    street_address2 = models.CharField(max_length=80, blank=True)
+    county = models.CharField(max_length=80, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.full_name}"
