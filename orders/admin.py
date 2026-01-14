60eb82b3291139e5c673195da3984e28f8df28f8
mariebernf <77eyo7ks@students.codeinstitute.net>
2026-01-14 21:03:55 +0100

    Configure Order Line Item as inline in order admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
-admin.site.register(Order)
-admin.site.register(OrderLineItem)
+
+class OrderLineItemInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('product', 'quantity', 'size')
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemInline,)
+    list_display = (
+        'id',
+        'full_name',
+        'email',
+        'order_total',
+        'date',
+    )
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
