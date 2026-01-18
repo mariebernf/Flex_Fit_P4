b159a12b41496e3070aa7c369a46320ef8973f98
mariebernf <77eyo7ks@students.codeinstitute.net>
2026-01-18 12:22:59 +0100

    Add custom 403 and 404 error pages

--- main/urls.py
+++ main/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler403, handler404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,3 +34,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'pages.views.custom_404'
+handler403 = 'pages.views.custom_403'
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/home.html')
+
+def custom_404(request, exception):
+    return render(request, 'errors/404.html', status=404)
+
+def custom_403(request, exception=None):
+    return render(request, 'errors/403.html', status=403)
