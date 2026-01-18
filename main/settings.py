fd07e24d30247b7d54c64c08ed5a21c95e2bcdb3
mariebernf <77eyo7ks@students.codeinstitute.net>
2026-01-18 11:38:46 +0100

    Set login redirect URL to protect order history page

--- main/settings.py
+++ main/settings.py
@@ -147,6 +147,7 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
+LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
