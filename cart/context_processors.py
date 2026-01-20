1a71a8f992f20afe2ec533e68467b03eb17badb5
mariebernf <77eyo7ks@students.codeinstitute.net>
2026-01-20 01:59:06 +0100

    Fix cart context processor to handle different item types

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,18 +1,25 @@
 from products.models import Product
 
+
 def cart_contents(request):
     cart = request.session.get('cart', {})
     cart_items = []
     total = 0
     product_count = 0
 
-   
+
     products = Product.objects.filter(id__in=cart.keys())
 
     for product in products:
         item = cart[str(product.id)]
-        quantity = item['quantity']
-        size = item['size']
+
+ 
+        if isinstance(item, int):
+            quantity = item
+            size = None
+        else:
+            quantity = item.get('quantity')
+            size = item.get('size')
 
         total += product.price * quantity
         product_count += quantity
@@ -22,6 +29,7 @@ def cart_contents(request):
             'quantity': quantity,
             'size': size,
         })
+
 
     return {
         'cart_items': cart_items,
