306916e125e9f6d8e48091e1fcfff6d995ef1202
mariebernf <77eyo7ks@students.codeinstitute.net>
2026-01-27 00:20:57 +0100

    Fix custom 404

--- main/urls.py
+++ main/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler403, handler404
 
 
 urlpatterns = [
