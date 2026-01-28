f91e868b74387c39bd334a774a58e90f32414e9f
mariebernf <77eyo7ks@students.codeinstitute.net>
2026-01-28 23:33:36 +0100

    Update error handlers in urls

--- pages/views.py
+++ pages/views.py
@@ -17,3 +17,6 @@ def custom_404(request, exception):
 
 def custom_403(request, exception=None):
     return render(request, '403.html', status=403)
+
+def custom_500(request):
+    return render(request, '500.html', status=500)
