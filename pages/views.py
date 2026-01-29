a41607b03118ac67a69858b84cde70c6f9fc7a57
mariebernf <77eyo7ks@students.codeinstitute.net>
2026-01-29 01:19:44 +0100

    Add screenshots to docs

--- pages/views.py
+++ pages/views.py
@@ -15,8 +15,8 @@ def home(request):
 def custom_404(request, exception):
     return render(request, '404.html', status=404)
 
-def custom_403(request, exception=None):
+def custom_403(request, exception):
     return render(request, '403.html', status=403)
 
-def custom_500(request):
+def custom_500(request, exception):
     return render(request, '500.html', status=500)
