9e2182822dbd02516a10409605dc9f4d1ce55213
mariebernf <77eyo7ks@students.codeinstitute.net>
2026-01-30 14:49:04 +0100

    Fix pages views

--- pages/views.py
+++ pages/views.py
@@ -18,5 +18,5 @@ def custom_404(request, exception):
 def custom_403(request, exception):
     return render(request, '403.html', status=403)
 
-def custom_500(request, exception):
+def custom_500(request, exception=None):
     return render(request, '500.html', status=500)
