034d6ff1d736daf03c0259708504491b5ec28bd0
mariebernf <77eyo7ks@students.codeinstitute.net>
2026-03-22 19:25:54 +0100

    Create Review form and view

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from products.models import Product
+from .models import Review
+from .forms import ReviewForm
 
-# Create your views here.
+
+@login_required
+def add_review(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    
+
+    if Review.objects.filter(product=product, user=request.user).exists():
+        return redirect('products:product_detail', product_id=product.id)
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.product = product
+            review.user = request.user
+            review.save()
+            return redirect('products:product_detail', product_id=product.id)
+    else:
+        form = ReviewForm()
+
+    return render(request, 'reviews/add_review.html', {
+        'form': form,
+        'product': product
+    })
