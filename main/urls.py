cd814199d935eac899e7093bf532421ac6990cb1
mariebernf <77eyo7ks@students.codeinstitute.net>
2026-03-28 23:09:13 +0100

    Fix URL config error after lint cleanup, remove unused accounts urls

--- main/urls.py
+++ main/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
     path('products/', include('products.urls')),
     path('', include('pages.urls')),
